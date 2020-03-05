329537b9c8cc19552e7ca6104eca567085edf8b2
giulianobernardi <g.bernardi.mnos@gmail.com>
2020-03-05 13:53:51 +0000

    Added the Color h-cpp file pair

--- Color.h
+++ Color.h
@@ -1,6 +1,6 @@
 // UDT storing rgba color
 struct Color 
 {
-    Color (float r_, float g_, float b_, float a_) : r(r_), g(g_), b(b_), a(a_) {}
+    Color (float r_, float g_, float b_, float a_);
     float r, g, b, a;
 };
